44f488712226d3e381c094472fceb6e3563fd6af
NathanielWatife <awosanminathniel0@gmail.com>
2024-08-20 02:38:50 +0100

    Worked on basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
 from datetime import datetime
+import uuid
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -15,16 +14,11 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.updated_at = datetime.now()
-                
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.created_at = datetime.now()
-                
+            self.id = kwargs.get('id', str(uuid.uuid4()))
+            self.created_at = datetime.strptime(kwargs.get('created_at',
+                datetime.now().isoformat()), '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs.get('updated_at', 
+                datetime.now().isoformat()), '%Y-%m-%dT%H:%M:%S.%f')
             if '__class__' in kwargs:
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
@@ -45,7 +39,12 @@ class BaseModel:
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
+                        (str(type(self)).split('.')[-1]).split('\'')[0]})
+        if hasattr(self, 'created_at'):
+            if isinstance(self.created_at, str):
+                self.created_at = datetime.fromisoformat(self.created_at)
+            dictionary['created_at'] = self.created_at.isoformat()
+        else:
+            dictionary['created_at'] = None
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
